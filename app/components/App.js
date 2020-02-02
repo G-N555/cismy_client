b447f977a4cfaab3424c496f6b183852a6e24c9c
Go Nakano <g.nakano555@gmail.com>
2020-02-02 16:50:13 +0900

    clean up

--- app/components/App.js
+++ app/components/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import News from "./News/News";
+import Genre from "./Genre/Genre";
+import Area from "./Area/Area";
+import SearchBar from "./SearchBar/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Header</header>
+      <News />
+      <SearchBar />
+      <Area />
+      <Genre />
     </div>
   );
 }
